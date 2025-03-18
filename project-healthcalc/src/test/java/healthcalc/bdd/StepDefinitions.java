de37d4fda2fd665ddbf060fc2b2578ce53827c60
GabiMY <gabimilenova04@gmail.com>
2025-03-18 14:07:14 +0100

    test: Implementation Cucumber

---
+++ project-healthcalc/src/test/java/healthcalc/bdd/StepDefinitions.java
@@ -0,0 +1,14 @@
+package healthcalc.bdd;
+import org.junit.jupiter.api.Assertions;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+//public class StepDefinitions, para rellenarla hacemos un .feature y ejecuatmos el mvn test
+public class StepDefinitions {
+	
+
+}
