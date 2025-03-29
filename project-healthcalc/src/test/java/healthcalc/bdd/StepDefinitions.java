507ae66610a965aa97b09411380d0ec9f57c77b2
GabiMY <gabimilenova04@gmail.com>
2025-03-29 21:11:24 +0100

    Update: Definition tests

--- project-healthcalc/src/test/java/healthcalc/bdd/StepDefinitions.java
+++ project-healthcalc/src/test/java/healthcalc/bdd/StepDefinitions.java
@@ -1,7 +1,4 @@
 package healthcalc.bdd;
-import org.junit.jupiter.api.Assertions;
-
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,6 +6,84 @@ import io.cucumber.java.en.When;
 
 //public class StepDefinitions, para rellenarla hacemos un .feature y ejecuatmos el mvn test
 public class StepDefinitions {
-	
+	@Given("La altura introducida es mayor a {int} metros")
+    public void la_altura_introducida_es_mayor_a_metros(Integer altura) {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("Introduzco los datos correctos")
+    public void introduzco_los_datos_correctos() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("el genero introducido es distinto de M")
+    public void el_genero_introducido_es_distinto_de_m() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("el genero es distinto de H")
+    public void el_genero_es_distinto_de_h() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("el genero no tiene valor")
+    public void el_genero_no_tiene_valor() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("edad mayor a {int} años")
+    public void edad_mayor_a_años(Integer edad) {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("el peso introducir es menor a 300kg")
+    public void el_peso_introducir_es_menor_a_300kg() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("el peso es mayor a {int} kg")
+    public void el_peso_es_mayor_a_kg(Integer peso) {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("presiono el boton calcular")
+    public void presiono_el_boton_calcular() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Se muestra por pantalla un error de altura incorrecta")
+    public void se_muestra_por_pantalla_un_error_de_altura_incorrecta() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Se muestra el resultado de la funcion peso ideal por pantalla")
+    public void se_muestra_el_resultado_de_la_funcion_peso_ideal_por_pantalla() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("El error se muestra por pantalla que el genero es invalido")
+    public void el_error_se_muestra_por_pantalla_que_el_genero_es_invalido() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("se muestra un error por la pantalla indicando que el genero es obligatorio")
+    public void se_muestra_un_error_por_la_pantalla_indicando_que_el_genero_es_obligatorio() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Se muestra por pantalla un error en la edad introducida")
+    public void se_muestra_por_pantalla_un_error_en_la_edad_introducida() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Se muestra por pantalla el resultado de la funcion calcular tasa metabolica")
+    public void se_muestra_por_pantalla_el_resultado_de_la_funcion_calcular_tasa_metabolica() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Se muestra el resultado de la funcion calcular tasa metabolica por pantalla")
+    public void se_muestra_el_resultado_de_la_funcion_calcular_tasa_metabolica_por_pantalla() {
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
