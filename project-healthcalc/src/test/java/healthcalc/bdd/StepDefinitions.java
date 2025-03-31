a1dc72272065d1545ba400c8537c66e87d1fbb4b
GabiMY <gabimilenova04@gmail.com>
2025-03-31 11:57:31 +0200

    Fix:Probelms implementation

--- project-healthcalc/src/test/java/healthcalc/bdd/StepDefinitions.java
+++ project-healthcalc/src/test/java/healthcalc/bdd/StepDefinitions.java
@@ -1,118 +1,122 @@
 package healthcalc.bdd;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static healthcalc.HealthCalc.*;
 
 import org.junit.jupiter.api.Assertions;
 
-import healthcalc.HealthCalc;
+import healthcalc.HealthCalcImpl;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-
-//public class StepDefinitions, para rellenarla hacemos un .feature y ejecuatmos el mvn test
 public class StepDefinitions {
-
+    private HealthCalcImpl calc;
+    private float result;
+    private boolean raisedException;
     private char genero;
     private int altura;
     private int edad;
     private float peso;
-    private String resultado;
-    private boolean raiseException ;
 
     @Before
-	public void initialization() {
-		genero = '\0';
+    public void setup() {
+        calc = new HealthCalcImpl();
+        result = 0;
+        raisedException = false;
+        genero = ' ';
         altura = 0;
-        edad = 0;  
+        edad = 0;
         peso = 0;
-        resultado = null;
-        raiseException = false; 
-	}
-
-	@Given("La altura introducida es mayor a {int} metros")
-    public void la_altura_introducida_es_mayor_a_metros(Integer altura) {
-        this.genero = 'H';
-        this.altura = altura + 1; 
     }
 
-    @Given("Introduzco los datos correctos")
-    public void introduzco_los_datos_correctos() {
-        this.genero = 'H';
-        this.altura = 175;
+    @Given("Tengo una calculadora de salud")
+    public void tengo_una_calculadora_de_salud() {
+        calc = new HealthCalcImpl();
     }
 
-    @Given("el genero introducido es distinto de M")
-    public void el_genero_introducido_es_distinto_de_m() {
-        this.genero = 'G';
+    @Given("Introduzco el genero {string}")
+    public void introduzco_el_genero(String genero) {
+        //Tengo que ponerlo asi o sino me da error
+        this.genero = genero.trim().isEmpty() ? '\0' : genero.charAt(0);
     }
 
-    @Given("el genero es distinto de H")
-    public void el_genero_es_distinto_de_h() {
-        this.genero = 'G';
+    @Given("Introduzco la altura {string}")
+    public void introduzco_la_altura(String altura) {
+        this.altura=Integer.parseInt(altura);
+    }
+
+    @When("Calcular el peso ideal")
+    public void calcular_el_peso_ideal() {
+        try {
+            result = calc.idealWeight(altura, genero);
+        } catch (Exception e) {
+            raisedException = true;
+        }
+    }
+
+    @Then("El sistema lanza una excepcion indicando que el genero es incorrecto")
+    public void el_sistema_lanza_una_excepcion_indicando_que_el_genero_es_incorrecto() {
+        Assertions.assertTrue(raisedException);
+    }
+
+    @Then("El sistema lanza una excepcion indicando que la altura es incorrecta")
+    public void el_sistema_lanza_una_excepcion_indicando_que_la_altura_es_incorrecta() {
+        Assertions.assertTrue(raisedException);
+    }
+
+    @Then("Se muestra el resultado {double}")
+    public void se_muestra_el_resultado(Double res) {
+        Assertions.assertEquals(res, this.result, 0.1);
     }
 
     @Given("el genero no tiene valor")
     public void el_genero_no_tiene_valor() {
-        this.genero = '\0';
+        this.genero=' ';
     }
 
-    @Given("edad mayor a {int} años")
-    public void edad_mayor_a_años(Integer edad) {
-        this.edad = edad + 1;
+    @Then("El sistema lanza una excepcion indicando que el genero es obligatorio")
+    public void el_sistema_lanza_una_excepcion_indicando_que_el_genero_es_obligatorio() {
+        Assertions.assertTrue(raisedException);
     }
 
-    @Given("el peso introducir es menor a 300kg")
-    public void el_peso_introducir_es_menor_a_300kg() {
-        this.peso = 200;
+    @Given("Edad introducida es {string}")
+    public void edad_introducida_es(String edad) {
+        this.edad=Integer.parseInt(edad);
     }
 
-    @Given("el peso es mayor a {int} kg")
-    public void el_peso_es_mayor_a_kg(Integer peso) {
-        this.peso = peso + 100;
+    @Given("Introduzco el peso {string}")
+    public void introduzco_el_peso(String peso) {
+        this.peso=Integer.parseInt(peso);
     }
 
-    @When("presiono el boton calcular")
-    public void presiono_el_boton_calcular() {
-        //NO SE
-        if (edad > 0) {
-            this.resultado = basalMetabolicRate(peso, altura, edad, genero);
-        } else {
-            this.resultado = idealWeight(genero, altura);
+    @When("Calcular TasaMetabolica")
+    public void calcular_tasa_metabolica() {
+        try {
+            result = calc.basalMetabolicRate(peso, altura, edad, genero);
+        } catch (Exception e) {
+            raisedException = true;
         }
-    }    
-    public void se_muestra_por_pantalla_un_error_de_altura_incorrecta() {
-        Assertions.assertTrue(raiseException);
     }
 
-    @Then("Se muestra el resultado de la funcion peso ideal por pantalla")
-    public void se_muestra_el_resultado_de_la_funcion_peso_ideal_por_pantalla() {
-        assertTrue(resultado.contains("Peso ideal"), "El resultado no contiene 'Peso ideal'");
-        //NO SE
+    @Then("El sistema lanza una excepcion indicando que la edad es incorrecto")
+    public void el_sistema_lanza_una_excepcion_indicando_que_la_edad_es_incorrecto() {
+        Assertions.assertTrue(raisedException);
     }
 
-    @Then("El error se muestra por pantalla que el genero es invalido")
-    public void el_error_se_muestra_por_pantalla_que_el_genero_es_invalido() {
-        assertEquals("Error: genero invalido", resultado);
+    @Then("El sistema lanza una excepcion indicando que el peso es incorrecto")
+    public void el_sistema_lanza_una_excepcion_indicando_que_el_peso_es_incorrecto() {
+        Assertions.assertTrue(raisedException);
     }
 
-    @Then("se muestra un error por la pantalla indicando que el genero es obligatorio")
-    public void se_muestra_un_error_por_la_pantalla_indicando_que_el_genero_es_obligatorio() {
-        assertEquals("Error: genero obligatorio", resultado);
+    @Given("la edad no tiene valor")
+    public void la_edad_no_tiene_valor() {
+        this.edad=0;
     }
 
-    @Then("Se muestra por pantalla un error en la edad introducida")
-    public void se_muestra_por_pantalla_un_error_en_la_edad_introducida() {
-        assertEquals("Error: Edad invalida", resultado);
+    @Then("se muestra un error por la pantalla indicando que la edad es obligatorio")
+    public void se_muestra_un_error_por_la_pantalla_indicando_que_la_edad_es_obligatorio() {
+        Assertions.assertTrue(raisedException);
     }
 
 
-    @Then("Se muestra el resultado de la funcion calcular tasa metabolica por pantalla")
-    public void se_muestra_el_resultado_de_la_funcion_calcular_tasa_metabolica_por_pantalla() {
-        assertTrue(resultado.contains("Tasa metabolica"), "El resultado no contiene 'Tasa metabolica'");
-        // NO SE
-    }
-
+    
 }
