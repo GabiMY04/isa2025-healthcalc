7c7ea414446957c99c89973b1956aea878de8886
GabiMY <gabimilenova04@gmail.com>
2025-05-19 15:57:31 +0200

    Feat: Implementation HealthAdapter Class

---
+++ project-healthcalc/src/main/java/healthcalc/HealthAdapter.java
@@ -0,0 +1,26 @@
+package healthcalc;
+
+public class HealthAdapter implements HealthHospital{
+    private HealthCalc calc;
+    public HealthAdapter() {
+        calc = HealthCalcImpl.getInstance();
+    }
+
+    @Override
+    public float pesoIdeal(char gender, float altura) throws Exception {
+        //Altura en metros
+        float altura1 = altura / 100;
+        return calc.idealWeight((int) altura1, gender);
+
+    }
+
+    @Override
+    public float bmr(char genero, int edad, float altura, int peso) throws Exception {
+        //Peso en gramos
+        float peso1 = peso / 1000;
+        float altura1 = altura / 100;
+        return calc.basalMetabolicRate(peso1, (int) altura1, edad, genero);
+        
+    }
+
+}
