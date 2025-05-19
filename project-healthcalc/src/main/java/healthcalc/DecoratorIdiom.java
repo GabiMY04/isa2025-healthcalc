b066d64b3854e63d9afec12d0573c568adc3e525
GabiMY <gabimilenova04@gmail.com>
2025-05-19 19:05:30 +0200

    Feat: Implementation DecoratorIdiom class

---
+++ project-healthcalc/src/main/java/healthcalc/DecoratorIdiom.java
@@ -0,0 +1,19 @@
+package healthcalc;
+
+public class DecoratorIdiom implements HealthHospital{
+    private HealthHospital hospital;
+    public DecoratorIdiom(HealthHospital hospital) {
+        this.hospital = hospital;
+    }
+
+    @Override
+    public float pesoIdeal(char gender, float altura) throws Exception {
+        return hospital.pesoIdeal(gender, altura);
+    }
+
+    @Override
+    public float bmr(char genero, int edad, float altura, int peso) throws Exception {
+        return hospital.bmr(genero, edad, altura, peso);
+    }
+
+}
