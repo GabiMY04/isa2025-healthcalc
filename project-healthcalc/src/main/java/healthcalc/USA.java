5baf16d2fd70b55516bd6adee27b32e155540bbb
GabiMY <gabimilenova04@gmail.com>
2025-05-19 19:46:17 +0200

    Feat: Implementation subclass USA of DecoratorRegion

---
+++ project-healthcalc/src/main/java/healthcalc/USA.java
@@ -0,0 +1,22 @@
+package healthcalc;
+public class USA extends DecoratorRegion{
+    public USA(HealthHospital hospital) {
+        super(hospital);
+    }
+    @Override
+    public float pesoIdeal(char gender, float altura) throws Exception {
+        return super.pesoIdeal(gender, altura);
+    }
+
+    @Override
+    public float bmr(char genero, int edad, float altura, int peso) throws Exception {
+        //Es necesario cambiar la variable hospital a protected para poder acceder a ella
+        HealthHospital espanyol = new Espanyol(super.hospital);
+        HealthHospital ingles = new Ingles(super.hospital);
+        espanyol.bmr(genero, edad, altura, peso);
+        ingles.bmr(genero, edad, altura, peso);
+
+        return super.bmr(genero, edad, altura, peso);
+
+    }
+}
