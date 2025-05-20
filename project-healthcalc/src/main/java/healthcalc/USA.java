26f0459dd7c805f86c31e175c862fd2b285e8592
GabiMY <gabimilenova04@gmail.com>
2025-05-20 17:24:07 +0200

    Fix: Eliminating errors in USA class

--- project-healthcalc/src/main/java/healthcalc/USA.java
+++ project-healthcalc/src/main/java/healthcalc/USA.java
@@ -1,23 +1,22 @@
 package healthcalc;
 public class USA extends DecoratorRegion{
+    //otra americana (que acepte la altura en pies y el peso en libras).
     public USA(HealthHospital hospital) {
         super(hospital);
     }
     @Override
     public float pesoIdeal(char gender, float altura) throws Exception {
-        float alturaPies = (float) (altura * 3.28084);
-        return super.pesoIdeal(gender, alturaPies);
+        //float alturaPies = (float) (altura * 3.28084);
+        float metros = altura * 0.3048f;
+        return super.pesoIdeal(gender, metros);
     }
 
     @Override
     public float bmr(char genero, int edad, float altura, int peso) throws Exception {
-        //Es necesario cambiar la variable hospital a protected para poder acceder a ella
-        float alturaPies = (float) (altura * 3.28084);
-        int pesoLibras = (int) (peso / 453.59237);
-        HealthHospital espanyol = new Espanyol(super.hospital);
-        HealthHospital ingles = new Ingles(super.hospital);
-        espanyol.bmr(genero, edad, altura, peso);
-        return ingles.bmr(genero, edad, alturaPies, pesoLibras);
+
+        float metros = altura * 0.3048f;
+        int gramos = (int) (peso * 453.59237f);
+        return super.bmr(genero, edad, metros, gramos);
 
     }
 }
