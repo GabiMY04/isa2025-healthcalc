6214039532d01fc9ffbf58594776a1c0b7cee744
GabiMY <gabimilenova04@gmail.com>
2025-05-20 12:05:51 +0200

    Fix: double calls in EU Class

--- project-healthcalc/src/main/java/healthcalc/EU.java
+++ project-healthcalc/src/main/java/healthcalc/EU.java
@@ -15,10 +15,10 @@ public class EU extends DecoratorRegion{
         //Es necesario cambiar la variable hospital a protected para poder acceder a ella
         HealthHospital espanyol = new Espanyol(super.hospital);
         HealthHospital ingles = new Ingles(super.hospital);
-        espanyol.bmr(genero, edad, altura, peso);
+        float resultado_español= espanyol.bmr(genero, edad, altura, peso);
         ingles.bmr(genero, edad, altura, peso);
-
-        return super.bmr(genero, edad, altura, peso);
+        //Si devuelvo el resultado_español solo se llamara una vez y no me saldra dos veces
+        return resultado_español;
     }
 
 
