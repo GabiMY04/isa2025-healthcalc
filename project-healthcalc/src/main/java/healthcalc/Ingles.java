02ff1d1e327b3ab6e45bf91df7b061bfe47ec4dc
GabiMY <gabimilenova04@gmail.com>
2025-05-20 11:58:22 +0200

    Fix: Metric conversion errors in InglesClass

--- project-healthcalc/src/main/java/healthcalc/Ingles.java
+++ project-healthcalc/src/main/java/healthcalc/Ingles.java
@@ -13,10 +13,12 @@ public class Ingles extends DecoratorIdiom {
     //Como solo se especifica el mensaje para bmr pues en la clase pesoIdeal se queda igual
     @Override
     public float bmr(char genero, int edad, float altura, int peso) throws Exception {
+        //Se pasa el peso en gramos y la altura en metros y hay que pasarlas
         float resultado= super.bmr(genero, edad, altura, peso);
-        float libras =  (float) (peso * 2.20462);
+        float peso_kg = (float) (peso / 1000.0);
+        float libras = peso_kg * 2.20462f; 
         float pies = (float) (altura * 3.28084); //Me parece mas conveniente ya que es en ingles todo
-        System.out.printf("The person with a height of %.2f meters and a weight of %.2f lbs has a BMR of %.2f.%n", pies, libras, resultado);
+        System.out.printf("The person with a height of %.2f feats and a weight of %.2f lbs has a BMR of %.2f.%n", pies, libras, resultado);
         return resultado;
     }
 
