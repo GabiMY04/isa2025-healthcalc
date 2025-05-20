ba654d04b30bdbf451ad27c12d647eb78e68c76e
GabiMY <gabimilenova04@gmail.com>
2025-05-20 17:26:54 +0200

    Fix: Eliminating errors in Ingles class

--- project-healthcalc/src/main/java/healthcalc/Ingles.java
+++ project-healthcalc/src/main/java/healthcalc/Ingles.java
@@ -15,7 +15,13 @@ public class Ingles extends DecoratorIdiom {
     public float bmr(char genero, int edad, float altura, int peso) throws Exception {
         //SOLO EL MENSAJE , SIN MODIFICA LAS METRICAS DE ESO SE ENCARGA EU Y USA
         float resultado= super.bmr(genero, edad, altura, peso);
-        System.out.printf("The person with a height of %.2f feats and a weight of %.2f lbs has a BMR of %.2f.%n", altura, peso, resultado);
+        if (altura < 3) {
+            //(EU)
+            System.out.printf("The person with a height of %.2f meters and a weight of %.2f kilograms has a BMR of %.2f.%n", altura, (float)peso/1000, resultado);
+        } else {
+            //(USA), Que no hay gente de 3 metros
+            System.out.printf("The person with a height of %.2f feet and a weight of %.2f lbs has a BMR of %.2f.%n", altura, (float)peso, resultado);
+        }
         return resultado;
     }
 
