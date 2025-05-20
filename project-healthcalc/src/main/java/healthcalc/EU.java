ef069e2095eeddffd38b0bb13c8f168a1e86fc24
GabiMY <gabimilenova04@gmail.com>
2025-05-20 17:23:26 +0200

    Fix: Eliminating errors in EU class

--- project-healthcalc/src/main/java/healthcalc/EU.java
+++ project-healthcalc/src/main/java/healthcalc/EU.java
@@ -12,16 +12,7 @@ public class EU extends DecoratorRegion{
 
     @Override
     public float bmr(char genero, int edad, float altura, int peso) throws Exception {
-        //Es necesario cambiar la variable hospital a protected para poder acceder a ella
-        //Se supone que aqui lo que se pasa en altura y peso son metros y gramos
-        float alturaPies = (float) (altura * 3.28084);
-        int pesoLibras = (int) (peso / 453.59237);
-        HealthHospital espanyol = new Espanyol(super.hospital);
-        HealthHospital ingles = new Ingles(super.hospital);
-        // Esta deberia de mostrarse con los medidas americanas
-        ingles.bmr(genero, edad, alturaPies, pesoLibras);
-        //Esta no la modificamos ya que se supone que es en metros y gramos
-        return espanyol.bmr(genero, edad, altura, peso);
+        return super.bmr(genero, edad, altura, peso);
     }
 
 
