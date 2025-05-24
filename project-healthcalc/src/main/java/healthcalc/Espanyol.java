3dc9e5c77cd1dd9910ed07eadd4681fe780936d2
GabiMY <gabimilenova04@gmail.com>
2025-05-24 13:54:17 +0200

    Update: Changing paremters to Person in Espanyol

--- project-healthcalc/src/main/java/healthcalc/Espanyol.java
+++ project-healthcalc/src/main/java/healthcalc/Espanyol.java
@@ -6,21 +6,21 @@ public class Espanyol extends DecoratorIdiom {
         super(hospital);
     }
     @Override
-    public float pesoIdeal(char gender, float altura) throws Exception {
-        float resultado = super.pesoIdeal(gender, altura);
+    public float pesoIdeal(Person person) throws Exception {
+        float resultado = super.pesoIdeal(person);
         return resultado;
     }
     //Como solo se especifica el mensaje para bmr pues en la clase pesoIdeal se queda igual
     @Override
-    public float bmr(char genero, int edad, float altura, int peso) throws Exception {
-        float resultado= super.bmr(genero, edad, altura, peso);
-        if (altura < 3) {
+    public float bmr(Person person) throws Exception {
+        float resultado= super.bmr(person);
+        if (person.height() < 3) {
             // EU
-            float pesoKg = (float)peso / 1000f;
-            System.out.printf("La persona con una altura de %.2f metros y un peso de %.2f kg tiene un BMR de %.2f.%n", altura, pesoKg, resultado);
+            float pesoKg = (float)person.Weight() / 1000f;
+            System.out.printf("La persona con una altura de %.2f metros y un peso de %.2f kg tiene un BMR de %.2f.%n", person.height(), pesoKg, resultado);
         } else {
             // USA
-            System.out.printf("La persona con una altura de %.2f pies y un peso de %.2f libras tiene un BMR de %.2f.%n", altura, (float)peso, resultado);
+            System.out.printf("La persona con una altura de %.2f pies y un peso de %.2f libras tiene un BMR de %.2f.%n", person.height(), (float)person.Weight(), resultado);
         }
 
         return resultado;
