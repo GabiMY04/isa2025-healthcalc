e18b038740d8da937aebb88ab96bbc3fde0ac8ee
GabiMY <gabimilenova04@gmail.com>
2025-05-24 13:49:09 +0200

    Update: Changing paremters to Person in Ingles

--- project-healthcalc/src/main/java/healthcalc/Ingles.java
+++ project-healthcalc/src/main/java/healthcalc/Ingles.java
@@ -6,21 +6,21 @@ public class Ingles extends DecoratorIdiom {
         super(hospital);
     }
     @Override
-    public float pesoIdeal(char gender, float altura) throws Exception {
-        float resultado = super.pesoIdeal(gender, altura);
+    public float pesoIdeal(Person person) throws Exception {
+        float resultado = super.pesoIdeal(person);
         return resultado;
     }
     //Como solo se especifica el mensaje para bmr pues en la clase pesoIdeal se queda igual
     @Override
-    public float bmr(char genero, int edad, float altura, int peso) throws Exception {
+    public float bmr(Person person) throws Exception {
         //SOLO EL MENSAJE , SIN MODIFICA LAS METRICAS DE ESO SE ENCARGA EU Y USA
-        float resultado= super.bmr(genero, edad, altura, peso);
-        if (altura < 3) {
+        float resultado= super.bmr(person);
+        if (person.height() < 3) {
             //(EU)
-            System.out.printf("The person with a height of %.2f meters and a weight of %.2f kilograms has a BMR of %.2f.%n", altura, (float)peso/1000, resultado);
+            System.out.printf("The person with a height of %.2f meters and a weight of %.2f kilograms has a BMR of %.2f.%n", person.height(), (float)person.Weight()/1000, resultado);
         } else {
             //(USA), Que no hay gente de 3 metros
-            System.out.printf("The person with a height of %.2f feet and a weight of %.2f lbs has a BMR of %.2f.%n", altura, (float)peso, resultado);
+            System.out.printf("The person with a height of %.2f feet and a weight of %.2f lbs has a BMR of %.2f.%n", person.height(), (float)person.Weight(), resultado);
         }
         return resultado;
     }
