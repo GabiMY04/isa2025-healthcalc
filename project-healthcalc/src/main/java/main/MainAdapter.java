94ea150fba4dc0059ce79c9a5faf2f974022ea9b
GabiMY <gabimilenova04@gmail.com>
2025-05-24 16:05:19 +0200

    Update: Changing parameters to  Person Class in MainAdapter

--- project-healthcalc/src/main/java/main/MainAdapter.java
+++ project-healthcalc/src/main/java/main/MainAdapter.java
@@ -1,15 +1,23 @@
 package main;
 
+import healthcalc.Gender;
 import healthcalc.HealthAdapter;
 import healthcalc.HealthHospital;
+import healthcalc.Person;
+import healthcalc.PersonImpl;
 
 public class MainAdapter {
     public static void main(String[] args) {
         // Main method to run the application
         try {
             HealthHospital prueba = new HealthAdapter();
-            System.out.println("Peso ideal: " + prueba.pesoIdeal('m', 1.60f)+ "kg");
-            System.out.println("BMR: " + prueba.bmr('m', 25, 1.60f, 70000) +" kcal");
+
+            Person personPeso = new PersonImpl(0, 1.60f, 0, Gender.MALE);
+
+            Person personBMR = new PersonImpl(70000, 1.60f, 25, Gender.MALE);
+            
+            System.out.println("Peso ideal: " + prueba.pesoIdeal(personPeso) + "kg");
+            System.out.println("BMR: " + prueba.bmr(personBMR) + " kcal");
         } catch (Exception e) {
             e.printStackTrace();
         }
