107f7d869f4fdc7257d0330ea4d8d0d77f6336f8
GabiMY <gabimilenova04@gmail.com>
2025-05-24 10:26:12 +0200

    Feat: Implementation MetabolicMetrics class

---
+++ project-healthcalc/src/main/java/healthcalc/MetabolicMetrics.java
@@ -0,0 +1,21 @@
+package healthcalc;
+
+public class MetabolicMetrics {
+    Person person;
+    HealthCalc healthCalc;
+
+    public MetabolicMetrics(Person person) {
+        this.person = new PersonImpl();
+        this.healthCalc = HealthCalcImpl.getInstance();
+    }
+    public double basalMetabolicRate() throws Exception {
+        char gender = (person.gender() == Gender.MALE) ? 'm' : 'w';
+        int age = person.age();
+        float weight = person.Weight();
+        int height = (int) person.height();
+        double res=healthCalc.basalMetabolicRate(weight, height, age, gender);
+        
+        return res;
+    }
+
+}
