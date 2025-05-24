d81430a8eb4ebfd6236611942988cbeadebe4499
GabiMY <gabimilenova04@gmail.com>
2025-05-24 16:29:53 +0200

    Update: Changing parameters to  Person Class in HealthCalcTest

--- project-healthcalc/src/test/java/healthcalc/HealthCalcTest.java
+++ project-healthcalc/src/test/java/healthcalc/HealthCalcTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-
 @DisplayName("Tests para la calculadora de salud.")
 public class HealthCalcTest {
 	private HealthCalc calc;
@@ -22,83 +20,76 @@ public class HealthCalcTest {
 	public void idealWeight_genero_invalido() {
 		//Arrage
 		calc= HealthCalcImpl.getInstance();
-		char gender='g';
-		int height=163;
+		Person p = new PersonImpl(0, 163, 0, null);
 		//Act
-		Exception exp=assertThrows(IllegalArgumentException.class, () -> calc.idealWeight(height, gender));
+		Exception exp = assertThrows(IllegalArgumentException.class, () -> calc.idealWeight(p));
+
 		//Assert
 		assertTrue(exp.getMessage().contains("Genero invalido"));
 	}
 	@Test
 	@DisplayName("Comprobacion para genero  invalido en la funcion basalMetabolicRate ")
 	public void basalMetabolicRate_genero_invalido() {
-		//Arrage
-		calc= HealthCalcImpl.getInstance();
-		char gender='g';
-		int height=163;
-		int age=18;
-		float weight= 60;
-		//Act
-		Exception exp=assertThrows(IllegalArgumentException.class, () -> calc.basalMetabolicRate(weight,height,age, gender));
-		//Assert
-		assertTrue(exp.getMessage().contains("Genero invalido"));
+		//Arrange
+        calc = HealthCalcImpl.getInstance();
+        Person p = new PersonImpl(60, 163, 18, null);
+        //Act
+        Exception exp = assertThrows(IllegalArgumentException.class, 
+            () -> calc.basalMetabolicRate(p));
+        //Assert
+        assertTrue(exp.getMessage().contains("Genero invalido"));
+
 	}
 	@Test
 	@DisplayName("Comprobacion altura invalida en la fucnion idealWeight")
 	public void idealWeight_altura_invalido() {
-		//Arrage
-		calc= HealthCalcImpl.getInstance();
-		char gender='w';
-		int height=500;
-		//Act
-		Exception exp=assertThrows(IllegalArgumentException.class, () -> calc.idealWeight(height, gender));
-		//Assert
-		assertTrue(exp.getMessage().contains("Altura invalida"));
+		//Arrange
+        calc = HealthCalcImpl.getInstance();
+        Person p = new PersonImpl(60, 500, 20, Gender.FEMALE);
+        //Act
+        Exception exp = assertThrows(IllegalArgumentException.class, 
+            () -> calc.idealWeight(p));
+        //Assert
+        assertTrue(exp.getMessage().contains("Altura invalida"));
 	}
 
 	@Test
 	@DisplayName("Comprobacion altura invalida en la fucnion basalMetabolicRate")
 	public void basalMetabolicRate_altura_invalido() {
-		//Arrage
-		calc= HealthCalcImpl.getInstance();
-		char gender='w';
-		int height=500;
-		int age=18;
-		float weight= 60;
-		//Act
-		Exception exp=assertThrows(IllegalArgumentException.class, () -> calc.basalMetabolicRate(weight,height,age, gender));
-		//Assert
-		assertTrue(exp.getMessage().contains("Altura invalida"));
+		//Arrange
+        calc = HealthCalcImpl.getInstance();
+        Person p = new PersonImpl(60, 500, 18, Gender.FEMALE);
+        //Act
+        Exception exp = assertThrows(IllegalArgumentException.class, 
+            () -> calc.basalMetabolicRate(p));
+        //Assert
+        assertTrue(exp.getMessage().contains("Altura invalida"));
 	}
 
 	@Test
 	@DisplayName("Comprobacion edad invalida en la fucnion basalMetabolicRate")
 	public void basalMetabolicRate_edad_invalido() {
-		//Arrage
-		calc= HealthCalcImpl.getInstance();
-		char gender='w';
-		int height=163;
-		int age=300;
-		float weight= 60;
-		//Act
-		Exception exp=assertThrows(IllegalArgumentException.class, () -> calc.basalMetabolicRate(weight,height,age, gender));
-		//Assert
-		assertTrue(exp.getMessage().contains("Edad invalida"));
+		//Arrange
+        calc = HealthCalcImpl.getInstance();
+        Person p = new PersonImpl(60, 163, 300, Gender.FEMALE);
+        //Act
+        Exception exp = assertThrows(IllegalArgumentException.class, 
+            () -> calc.basalMetabolicRate(p));
+        //Assert
+        assertTrue(exp.getMessage().contains("Edad invalida"));
 	}
 
 	@Test
 	@DisplayName("Comprobacion peso  invalido en la fucnion basalMetabolicRate")
 	public void basalMetabolicRate_peso_invalido() {
-		//Arrage
-		calc= HealthCalcImpl.getInstance();
-		char gender='w';
-		int height=163;
-		int age=23;
-		float weight= 600;
-		//Act
-		Exception exp=assertThrows(IllegalArgumentException.class, () -> calc.basalMetabolicRate(weight,height,age, gender));
-		//Assert
-		assertTrue(exp.getMessage().contains("Peso invalido"));
+		//Arrange
+        calc = HealthCalcImpl.getInstance();
+        Person p = new PersonImpl(600, 163, 23, Gender.FEMALE);
+        //Act
+        Exception exp = assertThrows(IllegalArgumentException.class, 
+            () -> calc.basalMetabolicRate(p));
+        //Assert
+        assertTrue(exp.getMessage().contains("Peso invalido"));
 	}
 
 	@Test
@@ -106,10 +97,10 @@ public class HealthCalcTest {
 	public  void idealWeight_women() throws Exception{
 		//Arrage
 		calc= HealthCalcImpl.getInstance();
-		char gender='w';
 		int height=163;
 		//Act
-		float res= calc.idealWeight(height, gender);
+		Person person = new PersonImpl(0, 163, 0, Gender.FEMALE);
+		float res = calc.idealWeight(person);
 		float res2 = height - 100 - ((height - 150) / 2.5f);
 		//Assert
 		assertEquals(res2, res);
@@ -117,46 +108,47 @@ public class HealthCalcTest {
 	@Test
 	@DisplayName("calcular el peso ideal de un hombre")
 	public  void idealWeight_men() throws Exception{
-		//Arrage
-		calc= HealthCalcImpl.getInstance();
-		char gender='m';
-		int height=163;
-		//Act
-		float res= calc.idealWeight(height, gender);
-		float res2 = height - 100 - ((height - 150) / 4.0f);
-		//Assert
-		assertEquals(res2, res);
+		 //Arrange
+        calc = HealthCalcImpl.getInstance();
+        int height = 163;
+        Person p = new PersonImpl(0, height, 0, Gender.MALE);
+        //Act
+        float res = calc.idealWeight(p);
+        float res2 = height - 100 - ((height - 150) / 4.0f);
+        //Assert
+        assertEquals(res2, res);
 	}
 
 
 	@Test
 	@DisplayName("Comprobar  la IBM ideal de una mujer")
 	public  void basalMetabolicRate_women() throws Exception{
-		//Arrage
-		calc= HealthCalcImpl.getInstance();
-		char gender='w';
-		int height=163;
-		int age=20;
-		float weight= 60;
-		//Act
-		float res= calc.basalMetabolicRate(weight, height, age, gender);
-		float res2=447.593f + 9.247f * weight + 3.098f * height - 4.330f * age;
-		//Assert
-		assertEquals(res2, res);
+		//Arrange
+        calc = HealthCalcImpl.getInstance();
+        float weight = 60;
+        int height = 163;
+        int age = 20;
+        Person p = new PersonImpl(weight, height, age, Gender.FEMALE);
+        //Act
+        float res = calc.basalMetabolicRate(p);
+        float res2 = 447.593f + 9.247f * weight + 3.098f * height - 4.330f * age;
+        //Assert
+        assertEquals(res2, res);
 	}
 	@Test
 	@DisplayName("Comprobar la IBM ideal de un hombre")
 	public  void basalMetabolicRate_men() throws Exception{
-		//Arrage
-		calc= HealthCalcImpl.getInstance();
-		char gender='m';
-		int height=163;
-		int age=20;
-		float weight= 60;
-		//Act
-		float res= calc.basalMetabolicRate(weight, height, age, gender);
-		float res2=88.362f + 13.397f * weight + 4.799f * height - 5.677f * age;
-		//Assert
-		assertEquals(res2, res);
+		//Arrange
+        calc = HealthCalcImpl.getInstance();
+        float weight = 60;
+        int height = 163;
+        int age = 20;
+        Person p = new PersonImpl(weight, height, age, Gender.MALE);
+        //Act
+        float res = calc.basalMetabolicRate(p);
+        float res2=88.362f + 13.397f * weight + 4.799f * height - 5.677f * age;
+        //Assert
+        assertEquals(res2, res);
 	}
 }
+
