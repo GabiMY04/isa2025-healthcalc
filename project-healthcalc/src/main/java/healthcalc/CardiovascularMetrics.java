2771ff7c1335992e7f7493f160d046cc40f64505
GabiMY <gabimilenova04@gmail.com>
2025-05-24 10:21:03 +0200

    Feat:Implementation of PersonImpl in CardiovascularMetrics class

--- project-healthcalc/src/main/java/healthcalc/CardiovascularMetrics.java
+++ project-healthcalc/src/main/java/healthcalc/CardiovascularMetrics.java
@@ -1,5 +1,18 @@
 package healthcalc;
 
 public class CardiovascularMetrics {
+    Person person;
+    HealthCalc healthCalc;
 
+    public CardiovascularMetrics(Person person) {
+        this.person = new PersonImpl();
+        this.healthCalc = HealthCalcImpl.getInstance();
+    }
+    public double getIdealBodyWeight() throws Exception {
+        char gender = (person.gender() == Gender.MALE) ? 'm' : 'w';
+        int age = person.age();
+        double res=healthCalc.idealWeight(age, gender);
+        
+        return res;
+    }
 }
