3487f8a47519ac83df2137868d0345f3e95750c0
GabiMY <gabimilenova04@gmail.com>
2025-05-24 16:09:39 +0200

    Update: Changing parameters to  Person Class in MainDecorator

--- project-healthcalc/src/main/java/main/MainDecorator.java
+++ project-healthcalc/src/main/java/main/MainDecorator.java
@@ -2,11 +2,14 @@ package main;
 
 import healthcalc.EU;
 import healthcalc.Espanyol;
+import healthcalc.Gender;
 import healthcalc.HealthAdapter;
 import healthcalc.HealthCalc;
 import healthcalc.HealthCalcImpl;
 import healthcalc.HealthHospital;
 import healthcalc.Ingles;
+import healthcalc.Person;
+import healthcalc.PersonImpl;
 import healthcalc.USA;
 
 public class MainDecorator {
@@ -30,27 +33,31 @@ public class MainDecorator {
 
 
             char   genero = 'm';
+            Gender gender = genero == 'm' ? Gender.MALE : Gender.FEMALE;
             int    edad    = 30;
             float  metros = 1.70f;    //metros 
             int    gramos = 65000;    //gramos 
             float pies = (float) (metros * 3.28084); //pies
             int libras = (int) (gramos / 453.59237f); //libras
 
+            Person esp= new PersonImpl(gramos, metros, edad, gender);
+            Person ingl= new PersonImpl(libras, pies, edad, gender);
+
             System.out.println(" ------------ EU + Espanyol ------------ ");
-            euEspanyol.bmr(genero, edad, metros, gramos);
-            euEspanyol.pesoIdeal(genero, metros);
+            euEspanyol.bmr(esp);
+            euEspanyol.pesoIdeal(esp);
 
             System.out.println(" ------------ EU + Ingles ------------ ");
-            euIngles.bmr(genero, edad, metros, gramos);
-            euIngles.pesoIdeal(genero, metros);
+            euIngles.bmr(esp);
+            euIngles.pesoIdeal(esp);
 
             System.out.println(" ------------ USA + Espanyol ------------ ");
-            usaEspanyol.bmr(genero, edad, pies, libras);
-            usaEspanyol.pesoIdeal(genero, pies);
+            usaEspanyol.bmr(ingl);
+            usaEspanyol.pesoIdeal(ingl);
 
             System.out.println(" ------------ USA + Ingles ------------ ");
-            usaIngles.bmr(genero, edad, pies, libras);
-            usaIngles.pesoIdeal(genero, pies);
+            usaIngles.bmr(ingl);
+            usaIngles.pesoIdeal(ingl);
 
         } catch (Exception e) {
             e.printStackTrace();
